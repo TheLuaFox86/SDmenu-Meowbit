a3608465fe9f32fb481499f8da46387994797a5b
The LuaFox86 <peanuts005@outlook.com>
2025-01-01 22:25:52 +0000

    Add Picture Frame Program

---
+++ MeowbitSD/PictureFrame/main.py
@@ -0,0 +1,18 @@
+from meowbit import screen
+import os
+from time import sleep_ms
+os.chdir("pics")
+local pl = os.listdir()
+screen.sync = 0
+while 1:
+	try:
+		for p in pl:
+			screen.fill(0)
+			screen.loadBmp(p, -1, -1)
+			screen.refresh()
+			time.sleep_ms(5000)
+	
+	except:
+		screen.fill(0)
+		screen.text("Error")
+		screen.refresh()
